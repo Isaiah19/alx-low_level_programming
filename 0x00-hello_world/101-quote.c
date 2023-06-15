7840130c8d1de479a0335784ed3736ccea41690d
Isaiah19 <nwezeifeanyi93@gmail.com>
2023-06-15 11:57:29 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
-/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19".
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19".
  * followed by a newline, to standard error
- * Return, Always 1
+ * Return: Always 1.
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
