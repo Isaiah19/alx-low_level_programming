ad6e2c6cedd5fa044d3910f2f0da96cb004445ab
Isaiah19 <nwezeifeanyi93@gmail.com>
2023-06-16 09:07:07 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while
+		(n <= 122)
+		{
+			putchar(n);
+			n++;
+		}
+
+	return (0);
+}
