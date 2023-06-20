596471fa1071712c28c35e381f3ca618d293b23d
Isaiah19 <nwezeifeanyi93@gmail.com>
2023-06-20 05:12:26 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+}
