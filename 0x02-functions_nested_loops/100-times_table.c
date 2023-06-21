9e7f678003d18088be00a26e6983da15a94a6fcf
Isaiah19 <nwezeifeanyi93@gmail.com>
2023-06-20 17:36:00 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,14 +26,14 @@ void print_times_table(int n)
 	_putchar(' ');
 	if (prod >= 100)
 	{
-	_putchar((prod / 100) + 0);
-	_putchar(((prod / 10)) % 10 + 0);
+	_putchar((prod / 100) + '0');
+	_putchar(((prod / 10)) % 10 + '0');
 	}
 	else if (prod <= 99 && prod >= 10)
 	{
-	_putchar((prod / 10) + 0);
+	_putchar((prod / 10) + '0');
 	}
-	_putchar((prod % 10) + 0);
+	_putchar((prod % 10) + '0');
 	}
 	_putchar('\n');
 	}
